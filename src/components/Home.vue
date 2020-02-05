2172c158acea02f118caf5c7887b258ebdc8183f
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2020-02-05 22:08:34 +1100

    basic card setup

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,12 +7,17 @@
         v-for="country in info"
         :key="country.id"
         >
-          <img :alt="country.name" src="country.flag" />
-          <h2>{{ country.name }}</h2>
-          <p>Population: {{ country.population }}</p>
-          <p>Region: {{ country.region }}</p>
-          <p>Capital: {{ country.capital }}</p>
-          <!-- {{ country }} -->
+          <div
+            class="image-wrapper"
+            :style="{ backgroundImage: 'url(' + country.flag + ')' }"
+          >
+          </div>
+          <div class="card-content">
+            <h2>{{ country.name }}</h2>
+            <p>Population: {{ country.population }}</p>
+            <p>Region: {{ country.region }}</p>
+            <p>Capital: {{ country.capital }}</p>
+          </div>
         </div>
     </div>
   </div>
@@ -36,4 +41,29 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+  .card-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+
+  .country-card {
+    -webkit-box-shadow: 3px 4px 16px 2px rgba(209,209,209,1);
+    -moz-box-shadow: 3px 4px 16px 2px rgba(209,209,209,1);
+    box-shadow: 3px 4px 16px 2px rgba(209,209,209,1);
+    flex-basis: 21%;
+    margin: 15px 0;
+  }
+
+  .image-wrapper {
+    background-position: center;
+    background-size: cover;
+    height: 150px;
+    width: 100%;
+  }
+
+  .card-content {
+    padding: 0 15px;
+  }
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,31 @@ module.exports = {
             fallback: 'style-loader',
             use: ['css-loader']
           })
+      },
+      {
+        test: /\.svg$/,
+        rules: [ {
+          oneOf: [ {
+            resourceQuery: /^\?vue-template/,
+            loader: 'vue-template-loader',
+            options: {
+              functional: true
+            }
+          }, {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]?[hash]'
+            }
+          } ]
+        }, {
+          loader: 'svgo-loader',
+          options: {
+            plugins: [
+              { removeViewBox: false },
+              { removeDimensions: true }
+            ]
+          }
+        } ]
       }
     ]
   }
